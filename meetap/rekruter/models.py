6841204ccfda5505fc5bd91a7b28890f77a41b21
Jakub <j.kozdrowicz@apdeveloper.com.pl>
2020-07-31 11:45:02 +0200

    Kawał dobrej roboty żeby nie poszedł na marne

--- meetap/rekruter/models.py
+++ meetap/rekruter/models.py
@@ -31,21 +31,95 @@ class User(AbstractBaseUser, PermissionsMixin):
      (MALE, 'male'),
      (FEMALE, 'female'),
     )
+    STRAIGHT = 0
+    GAY = 1
+    BI = 2
+    TRANS = 3
+    OTHER = 4
+    SEX_CHOICES = (
+        (STRAIGHT, "Straight"),
+        (GAY, "Gay"),
+        (BI, "Bi"),
+        (TRANS, "Trans"),
+        (OTHER, "Other"),
+    )
+    NO_DRINKING = 0
+    LET_OTHERS_DRINK = 1
+    DRINKS_SOME = 2
+    ALCAHOLIC = 3
+    ALCOHOL_CHOICES = (
+        (NO_DRINKING, "No drinking"),
+        (LET_OTHERS_DRINK, "Doesn't mind company"),
+        (DRINKS_SOME, "Drinks alcohol"),
+        (ALCAHOLIC, "Drinks heavily"),
+    )
+    NO_SMOKING = 0
+    LET_OTHERS_SMOKE = 1
+    SMOKES_SOME = 2
+    HEAVY_SMOKER = 3
+    TOBACCO_CHOICES = (
+        (NO_SMOKING, "No smoking"),
+        (LET_OTHERS_SMOKE, "Doesn't mind company"),
+        (SMOKES_SOME, "Smokes sometimes"),
+        (HEAVY_SMOKER, "Smokes heavily"),
+    )
     email = models.EmailField(_('email address'), unique=True)
-    first_name = models.CharField(_('first_name'), max_length=30, blank=True)
+    first_name = models.CharField(_('first_name'), max_length=30)
     date_joined = models.DateTimeField(_('date joined'))
     is_staff = models.BooleanField(_('staff status'), default=False,)
     is_active = models.BooleanField(_('active'), default=True)
     role_council = models.PositiveSmallIntegerField(
-        choices=ROLE_CHOICES, null=True, blank=True, default=1)
+        choices=ROLE_CHOICES, default=1)
     avatar = models.ImageField(upload_to='avatars', null=True, blank=True)
     gender = models.PositiveSmallIntegerField(
         choices=GENDERS, null=True, blank=True)
-    age = models.DateField(blank=True, null=True)
-    mnemo_login = models.CharField(_('mnemo_login'), max_length=11, unique=True)
+    age = models.DateField()
+    mnemo_login = models.CharField(_('mnemo_login'), max_length=11, unique=True, blank=True)
     karma = models.IntegerField(default=0)
+    sex_orientation = models.PositiveSmallIntegerField(
+        choices=SEX_CHOICES, null=True)
+    other_orientation = models.CharField(_('other_orientation'), max_length=30, blank=True)
+    sex_role_activity = models.IntegerField(null=True, blank=True)  # 0-100
+    sex_role_dominance = models.IntegerField(null=True, blank=True)  # 0-100
+    alcohol = models.PositiveSmallIntegerField(
+        choices=ALCOHOL_CHOICES, null=True)
+    tobacco = models.PositiveSmallIntegerField(
+        choices=TOBACCO_CHOICES, null=True)
+    # Inne używki - nie podlega wyszukiwaniu.
+    other_drugs = models.CharField(_('other_drugs'), max_length=300, blank=True)
+    # Numer telefonu. Opcjonalny
+    telephone = models.CharField(_('telephone'), max_length=30, blank=True)
+    # Inne formy kontaktu ze mną.
+    other_contact = models.CharField(_('other_contact'), max_length=300, blank=True)
+    # Sekcja "O mnie"
+    about_me = models.CharField(_('other_contact'), max_length=1500, blank=True)
+    # Zainteresowania w formacie csv
+    interests = models.CharField(_('other_contact'), max_length= 500, blank=True)
+    # Pokaż mi treści dla dorosłych.
+    showme_adultcontent = models.BooleanField(_('showme_adultcontent'), default=False,)
+    # Pokaż moją orientację seksualną.
+    showmy_sexorientation = models.BooleanField(_('showmy_sexorientation'), default=False,)
+    # Pokaż moją rolę w seksie.
+    showmy_sexrole = models.BooleanField(_('showmy_sexrole'), default=False,)
+    # Pokazuj imprezy komercyjne
+    showme_commercial = models.BooleanField(_('showme_commercial'), default=True,)
+    # Pokazuj imprezy typu "Open"
+    showme_massevents = models.BooleanField(_('showme_massevents'), default=True,)
+    # Wysyłaj mi informację o odrzuceniu/akceptacji mojej prośby o doł. do wyd.
+    sendme_inv_status_me = models.BooleanField(_('sendme_inv_status_me'), default=True,)
+    # Wysyłaj mi informację o odrzuceniu/akceptacji zaproszeń na wyd., które ja wysłałem.
+    sendme_inv_status_others = models.BooleanField(_('sendme_inv_status_others'), default=True,)
+    # Wysyłaj mi wiadomości o otrzymanych przeze mnie zaproszeniach na wyd.
+    sendme_invitations = models.BooleanField(_('sendme_invitations'), default=True,)
+    # Wysyłaj mi wiadomości o tym, że znajomy utworzył nowe wydarzenia.
+    sendme_friend_events = models.BooleanField(_('sendme_invitations'), default=True,)
+    # Wysyłaj mi informację o tym, że ktoś poprosił o dołączenie do mojego wyd.
+    sendme_join_request = models.BooleanField(_('sendme_invitations'), default=True,)
 
 
+
+    # location = Tutaj wstaw pole geolokalizacji. Do ogarnięcia.
+    # search_radius = integer field? do powyższego. W km. Domyślne 5
 
     objects = UserManager()
 
--- meetap/strona/views.py
+++ meetap/strona/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import (render, get_object_or_404 as G404)
-# from rekruter.models import User
+from rekruter.models import User
 from .models import (PageSkin as S, Blog as B)
 from .models import PageNames as P
 from meetap.settings import LANGUAGES as L
@@ -48,3 +48,24 @@ def blog(request, blog_id):
      skins=S, context=context)
     template = 'strona/blog.html'
     return render(request, template, context_lazy)
+
+
+# Login Required
+# Strona ustawień profilu
+def myprofile(request):
+    userdata = User.objects.get(
+     id=request.user.id)
+    if request.method == 'POST':
+        form = PartyForm(request.POST)
+        if form.is_valid():
+            form.save(userdata)
+            return redirect('staffpanel_c')
+    else:
+        form = PartyForm()
+        context = {
+         'udata': userdata,
+         }
+        pl = PortalLoad(P, L, Pbi, 1, Cmi, Cli, )
+        context_lazy = pl.lazy_context(skins=S, context=context)
+        template = 'forms/partymaker.html'
+        return render(request, template, context_lazy)
