36b5dbac57c2131a60d7078bb7ddf17c852c2789
Jacques <j.kozdrowicz@apdeveloper.com.pl>
2020-09-11 13:04:41 +0200

    Kaskadowe chowanie "checków"

--- meetap/meetap/static/assets/js/event_adult_switch.js
+++ meetap/meetap/static/assets/js/event_adult_switch.js
@@ -18,11 +18,23 @@ function toggleelement(class_name, row_name)
 }
 }
 
+function rollbackelement(class_name, row_name, row_name2)
+{
+  var x = document.getElementById(class_name);
+  var y = document.getElementById(row_name).checked;
+  let z = document.getElementById(row_name2);
+  if (y === false) {
+    z.checked = false;
+    x.style.display = 'none';
+  }
+}
+
 $(document).ready(function()
 {
  $('#id_is_adult_only').click(function() // Po zaznaczeniu zgody udostępnij przycisk wyślij.
  {
    toggleelement("id_is_adult_only_row", "id_is_adult_only");
+   rollbackelement("id_is_sex_party_row", "id_is_adult_only", "id_is_sex_party")
    hop();
 });
  $('#id_is_sex_party').click(function() // Po zaznaczeniu zgody udostępnij przycisk wyślij.
