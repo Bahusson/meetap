8439e2c2f4101ff865a125a0db8b20568499bc1c
Jacques <j.kozdrowicz@apdeveloper.com.pl>
2020-09-13 20:44:32 +0200

    checkpoint
    bujam się dalej i coś nie klika.

--- meetap/meetap/static/assets/js/event_remove_safelock.js
+++ meetap/meetap/static/assets/js/event_remove_safelock.js
@@ -1,6 +1,20 @@
-function toggleelement(class_name)
+/*function toggleelement(class_name, element_value, form_name)
 {
- var x = document.getElementsByClassName(class_name);
+ var x = document.getElementsByName(form_name)[element_value]
+ x.document.getElementsByClassName(class_name)[element_value];
+
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  }
+  else {
+    x.style.display = 'none';
+}
+}*/
+
+function toggleshort(class_name, element_value)
+{
+ var x = document.getElementsByName(class_name[element_value]
+
   if (x.style.display === 'none') {
     x.style.display = 'block';
   }
@@ -11,12 +25,14 @@ function toggleelement(class_name)
 
 $(document).ready(function()
 {
- $('.untoggle').click(function(event) // Po zaznaczeniu zgody udostępnij przycisk wyślij.
+var safety_valve = $("[name='safety_valve']");
+safety_valve.click(function(event); // Po zaznaczeniu zgody udostępnij przycisk wyślij.
  {
-   var element_value = $(event.target).value();
-   toggleelement(".toggle", element_value);
-   toggleelement(".untoggle", element_value);
-   hop();
+   var element_value = $(event.target).val();
+   alert(element_value);
+   toggleshort("untoggle", element_value);
+   toggleshort("toggle", element_value);
+
 });
 
 });
